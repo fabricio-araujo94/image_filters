6d2d6199390fd99dea689515c3f8d0b07eb05e78
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2026-04-14 19:56:43 -0300

    feat: add parameterization to the blur filter

--- src/filters.c
+++ src/filters.c
@@ -1,6 +1,7 @@
 #include "../include/filters.h"
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 static inline uint8_t clamp(int value) {
     if (value < 0) return 0;
@@ -57,43 +58,42 @@ void filter_contrast(Image *img, float factor) {
     }
 }
 
-void filter_mean_blur(Image *img) {
-    if (!img || !img->data) return;
+
+void filter_mean_blur(Image *img, int kernel_size) {
+    if (!img || !img->data || kernel_size < 3) return;
+
+    if (kernel_size % 2 == 0) kernel_size++;
 
     int w = img->width;
     int h = img->height;
     int channels = img->channels;
     int size = w * h * channels;
+    int offset = kernel_size / 2;
+    float num_pixels = (float) (kernel_size * kernel_size);
 
-    uint8_t *temp = (uint8_t *) malloc(size * sizeof(uint8_t));
+    uint8_t *temp = (uint8_t*) malloc(size);
     if (!temp) return;
 
-    uint8_t *src = img->data;
-    uint8_t *dst = temp;
-    for (int i = 0; i < size; i++) {
-        *(dst + i) = *(src + i);
-    }
+    memcpy(temp, img->data, size);
 
-    for (int y = 1; y < h - 1; y++) {
-        for (int x = 1; x < w - 1; x++) {
+    for (int y = offset; y < h - offset; y++) {
+        for (int x = offset; x < w - offset; x++) {
+
             for (int c = 0; c < channels; c++) {
-                int sum = 0;
+                long sum = 0;
 
-                for (int ky = -1; ky <= 1; ky++) {
-                    for (int kx = -1; kx <= 1; kx++) {
-                        int py = y + ky;
-                        int px = x + kx;
-    
-                        int neighbor_idx = (py * w + px) * channels + c;
-                        sum += temp[neighbor_idx];
+                for (int ky = -offset; ky <= offset; ky++) {
+                    for (int kx = -offset; kx <= offset; kx++) {
+                        int idx = ((y + ky) * w + (x + kx)) * channels + c;
+                        sum += temp[idx];
                     }
                 }
 
                 int current_idx = (y * w + x) * channels + c;
-                img->data[current_idx] = (uint8_t)(sum / 9);
+                img->data[current_idx] = (uint8_t) (sum / num_pixels);
             }
         }
-    } 
+    }
 
     free(temp);
 }
